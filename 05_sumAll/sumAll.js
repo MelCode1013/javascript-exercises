11eb3e567e72b9177e174a594e75dddc4223f787
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2022-11-08 09:41:05 -0500

    test for sumALL.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if ((typeof a === "string") || (typeof b === "string")) {
+    return 'ERROR'
+} else if (a < 0 || b < 0) {
+    return 'ERROR'
+} else if ((a >= 0) && (b >= 0)) {
+  let min = Math.min(a, b)
+  let max = Math.max(a, b)
+  let sum = 0
+  for (let j = min; j <= max; j++) //can be shortened to: for(; min < max; min++)
+      sum += j
+      return sum 
+} else {
+    return 'ERROR'
+}  
 
 };
 
